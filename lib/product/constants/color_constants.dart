5ec91da0a48d9fdb7cfbeae3422b8f60bf0f6b8f
Ceylin <caltepeceylin@gmail.com>
2023-05-11 00:20:45 +0300

    feat: create elevated button component

---
+++ lib/product/constants/color_constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+@immutable
+class ColorsConstants {
+  const ColorsConstants._();
+  static const Color blue_Color = Color.fromRGBO(84, 148, 245, 1);
+  static const Color black_color = Colors.black;
+}
