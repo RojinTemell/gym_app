6bf8eceb7c15a09d22ffd1ca35143d866fc04865
Ceylin <caltepeceylin@gmail.com>
2023-05-16 17:13:07 +0300

    feat: create floating action button component

---
+++ lib/product/widgets/floating_button_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:gym_app/product/constants/color_constants.dart';
+
+class FloatingActionButtonComponent extends StatelessWidget {
+  const FloatingActionButtonComponent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      onPressed: () {},
+      child: Icon(Icons.arrow_back),
+      backgroundColor: ColorsConstants.grey_Color,
+    );
+  }
+}
