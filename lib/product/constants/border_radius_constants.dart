6838ecfc4b6a4cff7610c1106be2bdc4777bb2ef
Ceylin <caltepeceylin@gmail.com>
2023-05-20 13:25:20 +0300

    Revert "Merge branch 'master' into create-card-component"
    This reverts commit 58316f75955ad8da28309336b6d11339aaeba8c2, reversing
    changes made to 222c822c6fadcb820f9da97c9a88ae1e4505b8a2.

---
+++ lib/product/constants/border_radius_constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+@immutable
+class BorderRadiusConstants {
+  const BorderRadiusConstants._();
+
+  static BorderRadius cardBorderRadius = BorderRadius.circular(16);
+}
