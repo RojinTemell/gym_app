474001ceda4ac6b8c271e8063f66de166136d89e
Ceylin <caltepeceylin@gmail.com>
2023-05-22 19:54:51 +0300

    feat: create second page

--- lib/product/constants/string_constants.dart
+++ lib/product/constants/string_constants.dart
@@ -11,4 +11,7 @@ class StringConstants {
   static const String secondPage1SubTitle = "START YOUR JOURNEY";
   static const String secondPage2Title = "CREATE A WORKOUT PLAN";
   static const String secondPage2SubTitle = "TO STAY FIT";
+  static const String secondPage3Title = "ACTION IS THE";
+  static const String secondPage3SubTitle = "KEY TO ALL SUCCESS";
+  static const String secondPage3ButtonText = "Start Now";
 }
