3ee8c146b73e7102424797bc404b209236da26e5
Ceylin <caltepeceylin@gmail.com>
2023-05-30 20:54:28 +0300

    refactor: change textfield

--- lib/product/widgets/text_input_widget.dart
+++ lib/product/widgets/text_input_widget.dart
@@ -2,22 +2,27 @@ import 'package:flutter/material.dart';
 import 'package:gym_app/product/index.dart';
 
 class TextInputComponent extends StatelessWidget {
-  TextInputComponent({Key? key, required this.type, this.isLast = false})
+  TextInputComponent(
+      {Key? key,
+      required this.type,
+      this.isLast = false,
+      required this.controller})
       : super(key: key);
   final TextFieldType type;
   bool isLast;
   final EdgeInsets padding =
       const EdgeInsets.symmetric(horizontal: 32, vertical: 5);
+  final TextEditingController controller;
 
   @override
   Widget build(BuildContext context) {
-    TextEditingController controller = TextEditingController();
     return Padding(
       padding: padding,
       child: TextFormField(
         textInputAction: isLast ? TextInputAction.done : TextInputAction.next,
         controller: controller,
-        obscureText: type == TextFieldType.password,
+        obscureText: type == TextFieldType.password ||
+            type == TextFieldType.passwordAgain,
         decoration: InputDecoration(labelText: type.value),
         validator: (value) {
           if (value == null || value.isEmpty) {
